86bf56ad99231dd898ed32b2f65f3bc73f953650
Steve Kim <dkim.steve@gmail.com>
2020-07-07 11:44:04 -0700

    added AuthState to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ContactState from "./context/contact/ContextState";
+import AuthState from "./context/auth/authState";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -8,19 +9,21 @@ import styles from "./App.module.css";
 
 function App() {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <>
-          <Navbar title="Contacts" />
-          <div className={styles.container}>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <>
+            <Navbar title="Contacts" />
+            <div className={styles.container}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 }
 
