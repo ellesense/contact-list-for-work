b6e1e9ead2df4137f285ea5182ff600b9f38e646
Steve Kim <dkim.steve@gmail.com>
2020-07-11 21:17:23 -0700

    added Details component (placeholder for now)

---
+++ client/src/components/Details.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Details.module.css";
+
+const Details = () => {
+  return (
+    <div className={styles.details}>
+      <h1>Details</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        hic? Consequuntur perferendis aut itaque, doloribus provident quos
+        voluptate, beatae magni dolores molestias natus harum animi dolore
+        aliquid doloremque inventore nostrum!
+      </p>
+    </div>
+  );
+};
+
+export default Details;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,7 +2,9 @@ import React, { useContext, useEffect } from "react";
 import AuthContext from "../../context/auth/authContext";
 import Contacts from "../contacts/Contacts";
 import ContactForm from "../contacts/ContactForm";
+import Details from "../Details";
 import ContactFilter from "../contacts/ContactFilter";
+import styles from "./Home.module.css";
 
 const Home = () => {
   const authContext = useContext(AuthContext);
@@ -12,13 +14,14 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <div>
+    <>
+      <div className={styles.home}>
         <ContactForm />
-        <ContactFilter />
+        <Details />
       </div>
+      <ContactFilter />
       <Contacts />
-    </div>
+    </>
   );
 };
 
